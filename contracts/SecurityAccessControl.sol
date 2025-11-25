4e4eb8fd8b30e94101fd04766827a2a068ae9e81
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-25 10:39:30 +0900

    Correct import paths allowed the contract to compile successfully

--- contracts/SecurityAccessControl.sol
+++ contracts/SecurityAccessControl.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
 
 contract SecurityAccessControl is AccessControl, Pausable, ReentrancyGuard {
